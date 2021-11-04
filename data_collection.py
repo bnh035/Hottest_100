a65b6fe44eece0eb6961212fa019181cd5d26baf
Brice <bricehilliard035@gmail.com>
2021-11-04 21:14:50 +1100

    Updated Documentation

--- data_collection.py
+++ data_collection.py
@@ -2,42 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """ Hottest 100 data collection
+Scrapes the TripleJ website to collect the yearly rankings and then attempting to find trends and 
+patterns that correlate to the positions of various songs.
 
-This file collects data from the JJJ Hottest 100 in 2 parts, these are:
-* Collect ranking list from each of the years that it has been running in it"s current form^
-* Collect data from Genius.com regarding release dates and some other data^^
-^ It is taken that the starting years is 1993 as that is what is available from:
-    http://www.abc.net.au/triplej/hottest100/archive
-^^ At the moment the current scope only includes release date, but could possibly expand to include more data
-# TODO: * Come up with an algorithm to search through Genius.com.
-
-    > Release date lies inside the HTML snippet:
-        <div class="metadata_unit metadata_unit--table_row">
-            <span class="metadata_unit-label">Release Date</span>
-            <span class="metadata_unit-info metadata_unit-info--text_only">January 10, 1989</span>
-        </div>
-
-TODO: Potentially look into BeautifulSoup, this has already been imported, but not used
-TODO: Possibly need to search through plain text to match with the current data. Could then also store this as a dict
-      that could be referred to later. The match would have to include special characters from umlaut.
+TODO List
+=========
+TODO: Scrape data from genius links. Look into BeautifulSoup to see if it may be useful.
+TODO: Find other sources for information on these songs.
+    * Is it possible to analyse the actual song?
+    * Get artist details
 TODO: Combine the data from Genius.com with the current dataframe containing the Hottest 100 data
-TODO: Standardise camel/snake case
-TODO: Create a function that reads from the files containing the relevant data. Should speed things up
-
-26/10/21 Update
-===============
-Update format of program:
-
-- wrap in a main function
-    - Check data
-        + Use current date to check and download data
-    - Create functions for separate processes
-    - Repair data
-        + Download/scrape missing data
-    - Process data
-    - Plot data
-    - Analyse data
-
+TODO: Collect functions into separate files that represent functionality
 """
 
 from bs4 import BeautifulSoup
@@ -69,19 +44,9 @@ __maintainer__ = "Brice Hilliard"
 __email__ = "bricehilliard035@gmail.com"
 __status__ = "Production"
 
-current_year = 2016  #: This is the last year that is archived as of 24/01/2018
-first_year = 1993  #: First of lists archived
-columns = ["Year", "Position", "Artist", "Song"]
-songDF = pd.DataFrame(columns=columns)
-link_list = []
-name_list = []
-
-# Setup pretty printer
-pp = pprint.PrettyPrinter(indent=4)
 
 # =========== Useful Functions ==================
 # Move to a utils file
-
 def find_between(s, first, last, search_string):
     """Find in the string "s", the substring between the 2 substrings "first" and "last"
 
@@ -89,7 +54,7 @@ def find_between(s, first, last, search_string):
     :param first: The initial substring that encloses the returned substring
     :param last: The last substring that encloses the returned substring
     :param search_string: A string that can be searched in the found string. If not used enter ""
-    :return: A list of all substringa that are enclosed by the first and last substrings
+    :return: A list of all substrings that are enclosed by the first and last substrings
     """
     try:
         link_text = re.findall("{}.*?{}".format(first, last), s, re.DOTALL)
@@ -103,10 +68,17 @@ def find_between(s, first, last, search_string):
         return ""
 
 
-def replace_from_list_strings(stringToReplace, listOfChars, replace_char):
-    for i in listOfChars:
-        stringToReplace = stringToReplace.replace(i, replace_char)
-    return(stringToReplace)
+def replace_from_list_strings(string_replace, list_chars, replace_char):
+    """ Replace a list of chars in a string
+    
+    :param string_replace: string that contains characters that need to be replaced
+    :param list_chars: list of characters to be replaces
+    :param replace_char: character that replaces the other
+    :return: string with characters replaced
+    """
+    for i in list_chars:
+        string_replace = string_replace.replace(i, replace_char)
+    return(string_replace)
 
 
 # =========== Data Cleaning Functions ===========
@@ -116,8 +88,7 @@ def replace_from_list_strings(stringToReplace, listOfChars, replace_char):
 # =========== Scraping Functions ================
 # Move to a scraping file
 def scrape_jjj():
-    """
-    Scrapes data from the JJJ website and returns a list of dictionaries of track data
+    """Scrapes data from the JJJ website and returns a list of dictionaries of track data
     """
     out_text = ""
     base_url = "https://www.abc.net.au/triplej/hottest100/archive/search/?year="
@@ -133,7 +104,7 @@ def scrape_jjj():
     
     # remove extraneous characters
     for i in list_data:
-        replace_from_list_strings(i, ["\r", "\n"], ")
+        replace_from_list_strings(i, ["\r", "\n"], "")
         out_text+=i+"\n"
     
     # Split string at ] to get track data. Album data can be accessed by using the second element in the split output.
@@ -145,6 +116,11 @@ def scrape_jjj():
 
 
 def check_url(test_url):
+    """ Check URL exists
+    
+    :param test_url: URL to be checked
+    :return: True if URL exists, False otherwise
+    """
     # Request page
     page = requests.get(test_url)
 
@@ -156,26 +132,37 @@ def check_url(test_url):
     return valid
 
 
-def prep_url(artist_name, artist_song):
+def prep_url(name, song):
+    """ prepare the url based on name and song
+    
+    :param name: artist name
+    :param song: song name
+    :return: a url to be tested
+    """
     # Create the genius URL
     base_url = "https://genius.com/"
     end_url = "-lyrics"
-    mid_url = artist_name+"-"+artist_song
+    mid_url = name+"-"+song
 
-    # Replace special chars in data
+    # Replace special chars in the URL
     mid_url = mid_url.replace(" ", "-")
     replace_list = ["/", ".", "{", "}", ",", ".", "!", "?", "'", "(", ")"]
     mid_url = replace_from_list_strings(mid_url, replace_list, "")
     mid_url = mid_url.replace("&", "and")
 
-    # Construct the full
+    # Construct the full URL
     genius_url = base_url+mid_url+end_url
     genius_url = genius_url.replace("--", "-")
     return genius_url
 
 
 def add_song_url(name, song):
-    # Prepare and check a url based on artist name and song
+    """ Applied to a DF to add a column with a URL based on the artist name and song
+
+    :param name: artist name
+    :param song: song name
+    :return: A string constructed from name and song or placeholder if URL not found
+    """
     url = prep_url(name, song)
     if check_url(url):
         out_url = "XX-"
@@ -185,6 +172,11 @@ def add_song_url(name, song):
 
 
 def add_missing_col(check_str):
+    """ Applied to a DF based on if a URL contains XX-
+    
+    :param check_str: the string that will be searched in
+    :return: Boolean if check_str contains XX-
+    """
     # Add a column that checks if the url contains "XX-"
     search_str = "XX-"
     if search_str in check_str:
@@ -194,6 +186,14 @@ def add_missing_col(check_str):
 
 
 def google_search(search_term, api_key, cse_id, **kwargs):
+        """Find in the string "s", the substring between the 2 substrings "first" and "last"
+
+    :param s: The main string that the search will be conducted through
+    :param first: The initial substring that encloses the returned substring
+    :param last: The last substring that encloses the returned substring
+    :param search_string: A string that can be searched in the found string. If not used enter ""
+    :return: A list of all substringa that are enclosed by the first and last substrings
+    """
     # Currently limited to 100 queries a day
     # Figure a more efficient way of searching google
     service = build("customsearch", "v1", developerKey=api_key)
@@ -203,25 +203,25 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     return url
 
 
-def search_for_links(name, song):
+def search_for_url(name, song):
+    """Find in the string "s", the substring between the 2 substrings "first" and "last"
+    
+    :param name: artist name
+    :param song: song name
+    :return: url that has been searched for
+    """
     # Enter google API and CSE IDs
     API_KEY = "YY"
     CSE_ID = "XX"
 
-    # Create the search term
-    my_query = f"{name} {song} genius"
-
-    # Looking for this term in the search result to confirm website
-    genius_head = "https://genius.com/"
-
-    # This is the default link for when the search is unsuccessful
-    link = "XX-"
+    my_query = f"{name} {song} genius"  # Create the search term
+    genius_head = "https://genius.com/" # Looking for this term in the search result to confirm website
+    link = "XX-" # This is the default link for when the search is unsuccessful
 
     # Search Google
     my_url = google_search(my_query, API_KEY, CSE_ID)
 
-    # Check if the first result is from Genius.com
-    if genius_head in my_url:
+    if genius_head in my_url: # Check if the first result is from Genius.com
         link = my_url
     
     # Create a dictionary to be added to the link cache file
@@ -245,6 +245,14 @@ def search_for_links(name, song):
 
 
 def update_df(name, song, url, update_df):
+    """ Applied to a DF to update the url column
+
+    :param name: artist name
+    :param song: song name
+    :param url: initial url
+    :param update_df: DF that the basis for the update
+    :return: the updated url
+    """
     # Update the song urls for rows with the matching artist and song title
     if (name in update_df["artist"].unique()) and (song in update_df["track"].unique()):
         try:
@@ -257,9 +265,12 @@ def update_df(name, song, url, update_df):
 
 
 # ======== Main Function ============
-
 def main(search_google):
-    # Start the timer
+    """Find in the string "s", the substring between the 2 substrings "first" and "last"
+
+    :param search_google: Boolean (True if google search is required)
+    :return: DF containing the results of the scraping
+    """
     start = datetime.now()
     print(f"start time: {start}")
     
@@ -286,21 +297,14 @@ def main(search_google):
         for start_row in range(0, missing_df.shape[0], 10):
             end_row  = min(start_row + 10, missing_df.shape[0])
             curr_df = missing_df.iloc[start_row:end_row, :]
-            curr_df["song_url"] = curr_df.apply(lambda x: search_for_links(x.artist, x.track), axis=1)
+            curr_df["song_url"] = curr_df.apply(lambda x: search_for_url(x.artist, x.track), axis=1)
     
     # Load the link cache file
     link_df = pd.read_csv("data/link_cache.csv", index_col=0)
-
-    # Update the main DF with the link DF
     hottest_100_DF["song_url"] = hottest_100_DF.apply(lambda x: update_df(x.artist, x.track, x.song_url, link_df), axis=1)
-
-    # Update the val_link column after updating the links
     hottest_100_DF["val_link"] = hottest_100_DF.apply(lambda x: add_missing_col(x.song_url), axis=1)
-
-    # Save the main DF as cache.csv
     hottest_100_DF.to_csv("data/cache.csv")
 
-    # Print the time that it took to complete
     print(f"time to complete: {datetime.now() - start}")
     return hottest_100_DF
 
